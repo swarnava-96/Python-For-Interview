e2b3adc5decd770b8a6636fdbde5d40f6bcfb292
Swarnava Mukherjee <75041273+swarnava-96@users.noreply.github.com>
2021-12-20 02:50:05 +0530

    Add files via upload

---
+++ reverse_integer.py
@@ -0,0 +1,9 @@
+# Python program to reverse a number
+
+n = int(input("Please give a number : "))
+print("Before reverse your number is : ", n)
+reverse = 0
+while n!=0:
+    reverse = reverse*10 + n%10
+    n = (n//10)
+print("After reverse : ", reverse)
